5901ba82d4b206d6164da5c9406fc48165af02ef
SUNDUS-THE-CODER <119600559+SUNDUS-THE-CODER@users.noreply.github.com>
2023-01-17 21:50:05 +0000

    add all required functions

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = '';
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toLocaleUpperCase();
+};
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+};
+
+function overwriteBestCustomer(value) {
+    bestCustomer = value;
+}
+
+function changeLeastFavoriteCustomer(value) {
+    leastFavoriteCustomer = value;
+}
